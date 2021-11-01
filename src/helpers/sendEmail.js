b6fd2b6afd6ee081778d02457ef86f6d61ed2503
Alejandro Montilla <amontilla@luckycycle.com>
2021-11-01 18:06:52 +0100

    Add emailjs and loader

--- src/helpers/sendEmail.js
+++ src/helpers/sendEmail.js
@@ -1,27 +1,43 @@
-import Swal from 'sweetalert2';
+import Swal from 'sweetalert2'
+import emailjs from 'emailjs-com'
 
-const API_URL = process.env.REACT_APP_API_URL;
+const EMAILJS_USER_ID = process.env.REACT_APP_EMAILJS_USER_ID
+const SERVICE_ID = process.env.REACT_APP_EMAILJS_SERVICE_ID
+const TEMPLATE_ID = process.env.REACT_APP_EMAILJS_TEMPLATE_ID
 
+emailjs.init(EMAILJS_USER_ID)
 export const sendEmail = async (data) => {
 	try {
-		const res = await fetch(`${API_URL}`, {
-			method: 'POST',
-			headers: {
-				'Content-Type': 'application/json',
+		Swal.fire({
+			title: 'Sending...',
+			showCancelButton: false,
+			showConfirmButton: false,
+			didOpen: () => {
+				Swal.showLoading()
 			},
-			body: JSON.stringify(data),
-		});
-
-		if (!res.ok) {
-			Swal.fire('Oh no, something when wrong, please try later', 'error');
-		}
-
-		Swal.fire(
-			'Message sent!',
-			'Thanks for contact me, I will answer you ASAP',
-			'success'
-		);
+			didClose: () => {
+				Swal.hideLoading()
+			},
+		})
+		await emailjs.send(SERVICE_ID, TEMPLATE_ID, data, EMAILJS_USER_ID)
+		Swal.fire({
+			title: 'Message sent!',
+			text: 'Thanks for contact me, I will answer you ASAP',
+			icon: 'success',
+			confirmButtonText: 'Ok',
+			didOpen: () => {
+				Swal.hideLoading()
+		   },
+		})
 	} catch (error) {
-		console.log(error);
+		Swal.fire({
+			title: 'Something went wrong',
+			html: '<h5>Please contact me directly at <strong>ale.montilla.dev@gmail.com</strong></h5>',
+			icon: 'error',
+			didOpen: () => {
+				 Swal.hideLoading()
+			},
+			confirmButtonText: 'Ok',
+		})
 	}
-};
+}
